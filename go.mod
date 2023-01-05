1b7ae9be1b8885da98e4580a6d4a3841b84d103c
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2023-01-05 14:45:46 +0700

    update dependency to v2.1.0

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/mkhoirurrizqi/app-say-hello
 
 go 1.19
 
-require github.com/mkhoirurrizqi/module-say-hello v1.1.0
+require github.com/mkhoirurrizqi/module-say-hello/v2 v2.1.0
--- main_say_hello.go
+++ main_say_hello.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/mkhoirurrizqi/module-say-hello"
+	"github.com/mkhoirurrizqi/module-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(module_say_hello.SayHello("rzq"))
+	fmt.Println(module_say_hello.SayHello("rzq", "Swiss"))
 }
