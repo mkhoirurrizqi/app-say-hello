87de3633fd254cf2316afd271b961dec63cd35c5
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2023-01-05 14:24:45 +0700

    init main app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mkhoirurrizqi/app-say-hello
+
+go 1.19
+
+require github.com/mkhoirurrizqi/module-say-hello v1.0.0 // indirect
---
+++ main_say_hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mkhoirurrizqi/module-say-hello"
+)
+
+func main() {
+	fmt.Println(module_say_hello.SayHello())
+}
